03355f6b0899b1c9de5f55414a9dd830ad7a96dc
Turnyur <chukunah@gmail.com>
2021-11-24 09:18:20 +0100

    adds update

--- functions.py
+++ functions.py
@@ -7,6 +7,15 @@ def printHello():
     print("Hello")
 
 
+def greet(*names):
+    """This function greets all
+    the person in the names tuple."""
+    print(type(names))
+    # names is a tuple with arguments
+    for name in names:
+        print("Welcome", name)
+
+
 num1 =input("Enter Number 1: ")
 num2 =input("Enter Number 2: ")
 
@@ -14,3 +23,4 @@ print("Returned Summation is: ", getSum(num1, num2))
 
 a = printHello
 a()
+greet("Monica", "Luke", "Steve", "John")
---
+++ lamba_function.py
@@ -0,0 +1,9 @@
+# Program to show the use of lambda functions
+double = lambda x: x * 2
+
+print(double(5))
+
+
+
+greet=lambda name: print("Hello, ", name)
+greet("Turnyur")
